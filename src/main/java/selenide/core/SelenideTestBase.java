88701b969d34ad0fe973c0a40c083b47eb8ff9e4
88SergiiOliinyk <sergii.oliinyk@thomascookonline.com>
2017-07-15 14:10:19 +0300

    WebDriver factory

--- src/main/java/selenide/core/SelenideTestBase.java
+++ src/main/java/selenide/core/SelenideTestBase.java
@@ -3,13 +3,25 @@ package selenide.core;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.testng.annotations.BeforeClass;
+
+import static org.openqa.selenium.remote.BrowserType.CHROME;
 
 public class SelenideTestBase {
 
+    private String browser = System.getProperty("browser", CHROME);
+
     @BeforeClass
     public void setUp() {
-        ChromeDriverManager.getInstance().setup();
-        Configuration.browser = WebDriverRunner.CHROME;
+        switch (browser) {
+            case WebDriverRunner.CHROME:
+                ChromeDriverManager.getInstance().setup();
+                break;
+            case WebDriverRunner.FIREFOX:
+                FirefoxDriverManager.getInstance().setup();
+                break;
+        }
+        Configuration.browser = browser;
     }
 }
